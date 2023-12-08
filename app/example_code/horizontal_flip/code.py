6972576f12c5b8d994c63d62d00e5e10dc0a34a8
ZeroAct <lhj5682@naver.com>
2023-12-08 10:41:10 +0900

    add example code to playground

---
+++ app/example_code/horizontal_flip/code.py
@@ -0,0 +1,2 @@
+# change `image`(cv2, BGR) as you want.
+image = image[:, ::-1]
--- app/src/page/playground_page.py
+++ app/src/page/playground_page.py
@@ -14,6 +14,13 @@ logger = logging.getLogger(__name__)
 
 
 class PlayGround(BasePage):
+    @property
+    def example_code_dir(self):
+        return "example_code"
+
+    def get_code_list(self):
+        return list(filter(lambda x: Path(x).is_dir(), Path(self.example_code_dir).glob("*")))
+
     def _image_viewer(self, title, image):
         st.subheader(title)
         h, w = image.shape[:2]
@@ -38,12 +45,27 @@ class PlayGround(BasePage):
 
     def render_code_input(self):
         st.subheader("Code Input")
-        st_ace(
-            value="# change `image`(cv2, BGR) as you want.\nimage = image",
-            height=300,
-            language="python",
-            key="playground_code",
-        )
+
+        with st.expander("Example Codes"):
+            code_dir_list = self.get_code_list()
+            code_name_list = [code.name for code in code_dir_list]
+            code_name = st.selectbox("Select Example Code", options=code_name_list)
+            if code_name:
+                code_path = Path(self.example_code_dir) / code_name / "code.py"
+                input_image_path = Path(self.example_code_dir) / code_name / "input.jpg"
+                output_image_path = Path(self.example_code_dir) / code_name / "output.jpg"
+                code_base = code_path.read_text()
+                st.text_area("Code", value=code_base, height=300, key="playground_code_example")
+                columns = st.columns(2)
+                with columns[0]:
+                    st.image(str(input_image_path), use_column_width=True, channels="BGR")
+                with columns[1]:
+                    st.image(str(output_image_path), use_column_width=True, channels="BGR")
+
+            if st.button("Apply Example Code"):
+                st.session_state.playground_code = st.session_state.playground_code_example
+
+        st.text_area("Code", height=300, key="playground_code")
 
         if st.button("Run"):
             try:
@@ -59,8 +81,34 @@ class PlayGround(BasePage):
             return
         self._image_viewer("Result Image", st.session_state.playground_result_image)
 
+    def render_register_code(self):
+        if "playground_result_image" not in st.session_state:
+            return
+        st.text_input(
+            "Register Code Name",
+            value=f"code_{len(self.get_code_list())}",
+            key="playground_code_name",
+        )
+        if st.button("Register"):
+            code_path = (
+                Path(self.example_code_dir) / st.session_state.playground_code_name / "code.py"
+            )
+            input_image_path = (
+                Path(self.example_code_dir) / st.session_state.playground_code_name / "input.jpg"
+            )
+            output_image_path = (
+                Path(self.example_code_dir) / st.session_state.playground_code_name / "output.jpg"
+            )
+            code_path.parent.mkdir(parents=True, exist_ok=True)
+            with open(code_path, "w") as f:
+                f.write(st.session_state.playground_code)
+            cv2.imwrite(str(input_image_path), st.session_state.playground_input_image)
+            cv2.imwrite(str(output_image_path), st.session_state.playground_result_image)
+            st.rerun()
+
     def render_content(self):
         self.render_upload_image()
         self.render_input_image()
         self.render_code_input()
         self.render_result_image()
+        self.render_register_code()
