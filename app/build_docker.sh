a78ff513e4610d1f1b4d7e32011d85d2e807d175
jaeminjeon <rmk1075@snuailab.ai>
2024-02-28 15:31:23 +0900

    fix: move docker related file to under app

---
+++ app/build_docker.sh
@@ -0,0 +1,3 @@
+version=$(python -c "import src; print(src.__version__)")
+echo "Building version $version"
+docker build --build-arg NO_CACHE_ARG=$(date +%s) --tag snuailab/waffle-app:$version --tag snuailab/waffle-app .
--- build_docker.sh
+++
@@ -1,3 +0,0 @@
-version=$(python -c "from app import src; print(src.__version__)")
-echo "Building version $version"
-docker build --build-arg NO_CACHE_ARG=$(date +%s) --tag snuailab/waffle-app:$version --tag snuailab/waffle-app ./app
